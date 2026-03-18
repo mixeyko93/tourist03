720070087e5f46696f431b516c8b7a683c2d3464
Mikhail <mikhail@example.com>
2026-03-18 21:10:08 +0300

    Починил серверный деплой

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,24 @@ set -eu
 
 SERVER="${DEPLOY_SERVER:-root@45.153.71.21}"
 PROJECT_PATH="${DEPLOY_PATH:-/opt/tourist03}"
-DEPLOY_CMD="${DEPLOY_CMD:-git pull && ./.venv/bin/pip install -r requirements.txt && systemctl restart tourist03-app tourist03-bot caddy}"
+RSYNC_RSH="${RSYNC_RSH:-ssh}"
+DEPLOY_CMD="${DEPLOY_CMD:-./.venv/bin/pip install -r requirements.txt && systemctl restart tourist03-app tourist03-bot caddy}"
 
-ssh "$SERVER" "cd \"$PROJECT_PATH\" && $DEPLOY_CMD"
+rsync -az --delete \
+  --exclude='.git' \
+  --exclude='.deploy.local' \
+  --exclude='.env' \
+  --exclude='.venv' \
+  --exclude='DerivedData' \
+  --exclude='__pycache__' \
+  --exclude='.DS_Store' \
+  --exclude='._*' \
+  --exclude='node_modules' \
+  --exclude='frontend/node_modules' \
+  --exclude='uploads' \
+  --exclude='cache' \
+  --exclude='__cache__' \
+  -e "$RSYNC_RSH" \
+  ./ "$SERVER:$PROJECT_PATH/"
+
+ssh "$SERVER" "chown -R tourist03:tourist03 \"$PROJECT_PATH\" && cd \"$PROJECT_PATH\" && $DEPLOY_CMD"
